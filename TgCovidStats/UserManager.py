f3d80f1ce3aeab58ad9489c4f0d59b9741c09f30
replydev <replydev@protonmail.com>
2021-02-12 13:24:08 +0100

    Probable fix for incorrect transaction

--- TgCovidStats/UserManager.py
+++ TgCovidStats/UserManager.py
@@ -1,73 +1,108 @@
-from sqlalchemy import create_engine,Column,Integer,String,Boolean
-from sqlalchemy.orm import sessionmaker,scoped_session
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import QueuePool
 import logging
 
 Base = declarative_base()
 
+
 class User(Base):
 
     __tablename__ = "user"
 
-    user_id = Column(Integer,primary_key=True)
-    selected_region = Column(Integer,default=0)
-    selected_province = Column(Integer,default=0)
-    send_notifications = Column(Boolean,default=True)
+    user_id = Column(Integer, primary_key=True)
+    selected_region = Column(Integer, default=0)
+    selected_province = Column(Integer, default=0)
+    send_notifications = Column(Boolean, default=True)
+
 
 class UserManager:
-
-    def __init__(self,username: str,password: str,database: str):
-        engine = create_engine("mysql+pymysql://{USER}:{PASS}@localhost/{DB}?host=localhost?port=3306".format(USER=username,PASS=password,DB=database), poolclass=QueuePool)
+    def __init__(self, username: str, password: str, database: str):
+        engine = create_engine("mysql+pymysql://{USER}:{PASS}@localhost/{DB}?host=localhost?port=3306".format(USER=username, PASS=password, DB=database),poolclass=QueuePool,)
         session_factory = sessionmaker(bind=engine)
         self.Session = scoped_session(session_factory)
         Base.metadata.create_all(engine)
 
     def get_all_users(self):
         session = self.Session()
-        users = session.query(User).all()
-        session.close()
-        return users
+        try:
+            users = session.query(User).all()
+        except Exception as e:
+            logging.error("An exception has occurred during query: {}".format(e))
+            session.rollback()
+            return None
+        finally:
+            session.close()
+            return users
 
-    def insert_user(self,user_id: int):
+    def insert_user(self, user_id: int):
         if self.user_exists(user_id):
             return
         session = self.Session()
-        logging.info("Adding user: %d",user_id)
-        session.add(User(user_id=user_id))
-        session.commit()
-        session.close()
-    
-    def user_exists(self,user_id: int):
+        try:
+            logging.info("Adding user: %d", user_id)
+            session.add(User(user_id=user_id))
+        except Exception as e:
+            logging.error(
+                "An exception has occurred during user registration: {}".format(e)
+            )
+            session.rollback()
+        finally:
+            session.commit()
+            session.close()
+
+    def user_exists(self, user_id: int):
         return self.get_user(user_id) is not None
 
-    def get_user(self,user_id: int):
+    def get_user(self, user_id: int):
         session = self.Session()
-        user = session.query(User).filter(User.user_id==user_id).first()
-        session.close()
-        return user
-    
-    def update_region(self,user_id: int,value: str):
+        try:
+            user = session.query(User).filter(User.user_id == user_id).first()
+        except Exception as e:
+            logging.error("An error has occurred during user query: {}".format(e))
+            session.rollback()
+            return None
+        finally:
+            session.close()
+            return user
+
+    def update_region(self, user_id: int, value: str):
         session = self.Session()
-        user = session.query(User).filter(User.user_id==user_id).first()
-        user.selected_region = value
-        session.commit()
-        session.close()
+        try:
+            user = session.query(User).filter(User.user_id == user_id).first()
+            user.selected_region = value
+        except Exception as e:
+            logging.error("An error has occurred during region update: {}".format(e))
+            session.rollback()
+        finally:
+            session.commit()
+            session.close()
 
-    def update_province(self,user_id: int,value: str):
+    def update_province(self, user_id: int, value: str):
         session = self.Session()
-        user = session.query(User).filter(User.user_id==user_id).first()
-        user.selected_province = value
-        session.commit()
-        session.close()
-    
-    def update_send_notifications(self,user_id: int, value: bool):
+        try:
+            user = session.query(User).filter(User.user_id == user_id).first()
+            user.selected_province = value
+        except Exception as e:
+            logging.error("An error has occurred during province update: {}".format(e))
+            session.rollback()
+        finally:
+            session.commit()
+            session.close()
+
+    def update_send_notifications(self, user_id: int, value: bool):
         session = self.Session()
-        user = user = session.query(User).filter(User.user_id==user_id).first()
-        user.send_notifications = value
-        session.commit()
-        session.close()
-    
-
-
-
+        try:
+            user = session.query(User).filter(User.user_id == user_id).first()
+            user.send_notifications = value
+        except Exception as e:
+            logging.error(
+                "An error has occurred during send_notification attribute update: {}".format(
+                    e
+                )
+            )
+            session.rollback()
+        finally:
+            session.commit()
+            session.close()
